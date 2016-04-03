51f878cbd058828c1b96a2cc4bd560762d9dffa4
Michael Smith <michael@spinda.net>
2016-04-03 01:04:36 -0700

    Use reactive-banana for FRP
    I would add reactive-banana-wx as a Cabal dependency, but it insists on
    having cabal-macosx installed as a Setup.hs dependency. Since proper
    Setup.hs dependency management hasn't made it into a Cabal release yet,
    this would require a manual package installation step which I'd like to
    avoid if possible. So, for now, I'm pulling it in as a submodule.
    
    On the other hand, I may actually need cabal-macosx for the editor to
    run on OS X. If so, I'll add it in as a required dependency and swap out
    the submodule for a Cabal dependency on reactive-banana-wx.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE RecursiveDo #-}
+
 import Data.Version
 
 import Graphics.UI.WX
+
+import Reactive.Banana
+import Reactive.Banana.WX
 
 import Paths_glyph_editor
 
@@ -8,5 +13,15 @@ main :: IO ()
 main = start $ do
   f <- frame [text := "Glyph Editor " ++ showVersion version]
   let l = label "Hello, World!"
-  set f [layout := minsize (sz 300 300) $ margin 10 $ column 5 [floatCenter l]]
+
+  let networkDescription :: MomentIO ()
+      networkDescription = mdo
+        let bLayout :: Behavior Layout
+            bLayout = pure $ minsize (sz 300 300) $ margin 10 $ column 5 $
+              [floatCenter l]
+
+        sink f [layout :== bLayout]
+
+  network <- compile networkDescription
+  actuate network
 
