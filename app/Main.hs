17119916b2eaa33b9b73201603e861f327655bed
Michael Smith <michael@spinda.net>
2016-04-04 02:52:41 -0700

    Edit / add points to strokes
    The code for this is a bit of a mess at the moment, but it works!

--- app/Main.hs
+++ app/Main.hs
@@ -19,13 +19,15 @@
 
 import qualified Codec.Picture as P
 
+import Control.Monad
+
 import Data.Version
 
 import qualified Diagrams.Prelude as D
 import Diagrams.Backend.Rasterific
 import Diagrams.Core.Compile
 
-import Graphics.UI.WX
+import Graphics.UI.WX as WX
 import Graphics.UI.WX.Draw
 import Graphics.UI.WX.Types
 
@@ -34,7 +36,7 @@ import Graphics.UI.WXCore.WxcClassesAL
 import Graphics.UI.WXCore.WxcClassTypes
 import Graphics.UI.WXCore.WxcTypes
 
-import Reactive.Banana
+import Reactive.Banana as B
 import Reactive.Banana.WX
 
 import Glyph.Render
@@ -53,46 +55,66 @@ main = start $ do
 
   let networkDescription :: MomentIO ()
       networkDescription = mdo
-        let bStroke :: Behavior Stroke
-            bStroke = pure $ Stroke
-              { strokeHead = []
-              , strokeBody = [ StrokeStep { strokeStepPoint = D.mkP2 0.1 0.1
-                                          , strokeStepWidth = 1
-                                          , strokeStepKind  = NormalStep
-                                          }
-                             , StrokeStep { strokeStepPoint = D.mkP2 0.25 0.25
-                                          , strokeStepWidth = 1
-                                          , strokeStepKind  = NormalStep
-                                          }
-                             , StrokeStep { strokeStepPoint = D.mkP2 0.0 0.5
-                                          , strokeStepWidth = 1
-                                          , strokeStepKind  = NormalStep
-                                          }
-                             , StrokeStep { strokeStepPoint = D.mkP2 0.3 0.3
-                                          , strokeStepWidth = 1
-                                          , strokeStepKind  = NormalStep
-                                          }
-                             ]
-              , strokeTail = []
-              , strokeStartCap = SharpCap
-              , strokeEndCap = SharpCap
-              }
+        eAddPoint <- event1' p click
+        ePoints <- accumE [] $ unions
+          [ (\p -> (++ [p])) <$> eAddPoint
+          ]
+        bPoints <- stepper [] ePoints
 
-        sink p [on paint :== paintStroke <$> bStroke]
+        let bStrokeSteps :: Behavior [StrokeStep]
+            bStrokeSteps = map mkStrokeStep <$> bPoints
+
+        let bStroke :: Behavior Stroke
+            bStroke = mkStroke <$> bStrokeSteps
+
+        sink p [on paint :== paintPanel <$> bStroke <*> bPoints]
+        reactimate $ repaint p <$ eAddPoint
+        reactimate $ print <$> eAddPoint
 
   network <- compile networkDescription
   actuate network
 
-paintStroke :: Stroke -> DC a -> Rect -> IO ()
-paintStroke stroke dc rect = do
+event1' :: w -> WX.Event w (a -> IO ()) -> MomentIO (B.Event a)
+event1' widget e = do
+  addHandler <- liftIO $ event1ToAddHandler' widget e
+  fromAddHandler addHandler
+
+event1ToAddHandler' :: w -> WX.Event w (a -> IO ()) -> IO (AddHandler a)
+event1ToAddHandler' widget e = do
+    (addHandler, runHandlers) <- newAddHandler
+    set widget [on e := runHandlers]
+    return addHandler
+
+mkStrokeStep :: WX.Point -> StrokeStep
+mkStrokeStep (Point x y) =
+  StrokeStep (D.mkP2 (fromIntegral x / 300) (1 - fromIntegral y / 300)) 1 NormalStep
+
+mkStroke :: [StrokeStep] -> Stroke
+mkStroke strokes = Stroke
+  { strokeHead = []
+  , strokeBody = strokes
+  , strokeTail = []
+  , strokeStartCap = SharpCap
+  , strokeEndCap = SharpCap
+  }
+
+paintPanel :: Stroke -> [WX.Point] -> DC a -> Rect -> IO ()
+paintPanel stroke points dc rect = do
   image <- imageCreateSized $ Size 300 300
   withPixelBuffer image $ copyImageToPixelBuffer rendered
   drawImage dc image (rectTopLeft rect) []
   imageDelete image
+  unless (null points) $ do
+    mapM_ (drawPointHandle dc 1) (init points)
+    drawPointHandle dc 2 $ last points
   where
-    diagram = renderStroke stroke
+    diagram = D.rectEnvelope (D.mkP2 0 0) (D.V2 1 1) $ renderStroke stroke
     rendered = renderDia Rasterific options diagram
     options = RasterificOptions $ D.dims $ D.V2 300 300
+
+drawPointHandle :: DC a -> Int -> Point -> IO ()
+drawPointHandle dc width (Point x y) =
+  circle dc (Point (x - 2) (y - 2)) 4 [color := green, penWidth := width]
 
 copyImageToPixelBuffer :: P.Image P.PixelRGBA8 -> PixelBuffer -> IO ()
 copyImageToPixelBuffer img pixbuf = mapM_ copyRow [0..P.imageHeight img - 1]
